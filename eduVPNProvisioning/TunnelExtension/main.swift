95b9778e0e279ceaa4f68076b9b0deab24c7afea
Roopesh Chander <roop@roopc.net>
2023-02-27 10:32:54 +0530

    Create and setup Xcode project

---
+++ eduVPNProvisioning/TunnelExtension/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  TunnelExtension
+//
+//  Created by Roopesh Chander on 24/02/23.
+//
+
+import Foundation
+import NetworkExtension
+
+autoreleasepool {
+    NEProvider.startSystemExtensionMode()
+}
+
+dispatchMain()
---
+++ eduVPNProvisioning/eduVPNProvisioning/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  eduVPNProvisioning
+//
+//  Created by Roopesh Chander on 24/02/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
