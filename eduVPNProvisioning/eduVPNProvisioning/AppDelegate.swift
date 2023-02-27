62c031497e186d25dc149cc130d38886b324af0a
Roopesh Chander <roop@roopc.net>
2023-02-27 11:00:47 +0530

    Install system extension on launch for now

--- eduVPNProvisioning/eduVPNProvisioning/AppDelegate.swift
+++ eduVPNProvisioning/eduVPNProvisioning/AppDelegate.swift
@@ -6,15 +6,13 @@
 //
 
 import Cocoa
+import SystemExtensions
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        beginSystemExtensionInstallation()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -28,3 +26,43 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 }
 
+extension AppDelegate {
+    func beginSystemExtensionInstallation() {
+        NSLog("beginSystemExtensionInstallation")
+        guard let appId = Bundle.main.bundleIdentifier else { fatalError("missing bundle id") }
+        let tunnelExtensionBundleId = "\(appId).TunnelExtension"
+        let request = OSSystemExtensionRequest.activationRequest(
+            forExtensionWithIdentifier: tunnelExtensionBundleId,
+            queue: DispatchQueue.main)
+        request.delegate = self
+        OSSystemExtensionManager.shared.submitRequest(request)
+    }
+}
+
+extension AppDelegate: OSSystemExtensionRequestDelegate {
+    func request(_ request: OSSystemExtensionRequest, actionForReplacingExtension existing: OSSystemExtensionProperties, withExtension ext: OSSystemExtensionProperties) -> OSSystemExtensionRequest.ReplacementAction {
+        NSLog("System Extension: Replacing \(existing.bundleShortVersion) with \(ext.bundleShortVersion)")
+        return .replace
+    }
+
+    func requestNeedsUserApproval(_ request: OSSystemExtensionRequest) {
+        NSLog("System Extension: Needs user approval")
+        NSApp.terminate(self)
+    }
+
+    func request(_ request: OSSystemExtensionRequest, didFinishWithResult result: OSSystemExtensionRequest.Result) {
+        if result == .completed {
+            NSLog("System Extension: Loading complete")
+        } else if result == .willCompleteAfterReboot {
+            NSLog("System Extension: Loading requires reboot")
+        } else {
+            NSLog("System Extension: OSSystemExtensionRequest code = \(result.rawValue)")
+        }
+        NSApp.terminate(self)
+    }
+
+    func request(_ request: OSSystemExtensionRequest, didFailWithError error: Error) {
+        NSLog("System Extension: Error: \(error)")
+        NSApp.terminate(self)
+    }
+}
