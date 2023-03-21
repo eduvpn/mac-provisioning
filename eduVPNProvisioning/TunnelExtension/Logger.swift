c794179e77c407e6737b0611df42c619f4efe0e5
Roopesh Chander <roop@roopc.net>
2023-03-21 11:24:42 +0530

    Add a placeholder Logger

---
+++ eduVPNProvisioning/TunnelExtension/Logger.swift
@@ -0,0 +1,14 @@
+//
+//  Logger.swift
+//  TunnelExtension
+//
+//  Created by Roopesh Chander on 21/03/23.
+//
+
+import Foundation
+
+class Logger {
+    func log(_ message: String) {
+        NSLog(message)
+    }
+}
